978785a397bad57d2af08151d981fc1c04debf5f
Anna Buczek <aania.buczek@gmail.com>
2020-05-22 12:07:11 +0200

    Build skeletor af a gallery section

---
+++ src/sections/Gallery/Gallery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Gallery.scss';
+
+const Gallery = () => {
+  return (
+    <section className="gallery">
+      <div className="gallery__container">
+        <h2 className="gallery__heading">
+          Our adventures on Instagram
+        </h2>
+        <div className="gallery__content"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
