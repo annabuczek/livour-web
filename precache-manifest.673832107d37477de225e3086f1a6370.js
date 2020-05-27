b632c27aeea6a0ceee703519429ca3a8767e238d
Anna Buczek <aania.buczek@gmail.com>
2020-05-27 17:51:08 +0200

    Updates

---
+++ precache-manifest.673832107d37477de225e3086f1a6370.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5500825a6aac2bf6665e1679a694d1",
+    "url": "/livour-web/index.html"
+  },
+  {
+    "revision": "acf70913172fd1a76592",
+    "url": "/livour-web/static/css/main.6ceb2c39.chunk.css"
+  },
+  {
+    "revision": "9b4c5653ea59b5166098",
+    "url": "/livour-web/static/js/2.669f4e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/livour-web/static/js/2.669f4e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf70913172fd1a76592",
+    "url": "/livour-web/static/js/main.90b5b43b.chunk.js"
+  },
+  {
+    "revision": "5dbfb70fc7c6abfa8bcc",
+    "url": "/livour-web/static/js/runtime-main.72ec591d.js"
+  },
+  {
+    "revision": "75ed9e8d6cdb9f997106d70940b6104d",
+    "url": "/livour-web/static/media/about-us.75ed9e8d.jpg"
+  },
+  {
+    "revision": "073fc89eacf46ffc41d8c65490728b9a",
+    "url": "/livour-web/static/media/airplane.073fc89e.svg"
+  },
+  {
+    "revision": "3af9b563ff78ab141f7c7a4918223714",
+    "url": "/livour-web/static/media/beach.3af9b563.svg"
+  },
+  {
+    "revision": "b77152111f04b4f8f1274a855436b0b4",
+    "url": "/livour-web/static/media/cambodia.b7715211.jpg"
+  },
+  {
+    "revision": "35fd0fcb582a95b38e49a511bacb016b",
+    "url": "/livour-web/static/media/facebook-white.35fd0fcb.svg"
+  },
+  {
+    "revision": "eb3589ec42054fd108aca36049a8246d",
+    "url": "/livour-web/static/media/facebook.eb3589ec.svg"
+  },
+  {
+    "revision": "ae6506ace502a135517edb79d3f0b070",
+    "url": "/livour-web/static/media/hot-air-balloon.ae6506ac.svg"
+  },
+  {
+    "revision": "f4ee0e0c5af7b8be45322d6edabaf3dd",
+    "url": "/livour-web/static/media/instagram-white.f4ee0e0c.svg"
+  },
+  {
+    "revision": "b94f114ea5e40babc2d94433b7000433",
+    "url": "/livour-web/static/media/instagram.b94f114e.svg"
+  },
+  {
+    "revision": "a9be969abc70c5be1b338e5eaca39ab8",
+    "url": "/livour-web/static/media/left-arrow.a9be969a.svg"
+  },
+  {
+    "revision": "65266eb4dfc174d682642f47afefc0bf",
+    "url": "/livour-web/static/media/main-banner.65266eb4.jpg"
+  },
+  {
+    "revision": "f71a911138e59a216c6748d1273af19e",
+    "url": "/livour-web/static/media/nepal.f71a9111.jpg"
+  },
+  {
+    "revision": "c7c0ce60b3752708eb7c693279464e38",
+    "url": "/livour-web/static/media/reviews-background.c7c0ce60.jpg"
+  },
+  {
+    "revision": "67975f6c68b16d4f2ef21180eb8c439e",
+    "url": "/livour-web/static/media/right-arrow.67975f6c.svg"
+  },
+  {
+    "revision": "aa6ff5362cf97598fa690039b77cfd4f",
+    "url": "/livour-web/static/media/sri-lanca.aa6ff536.jpg"
+  },
+  {
+    "revision": "71b402d1783fd24502a2e48e42fabfe3",
+    "url": "/livour-web/static/media/tourist.71b402d1.svg"
+  },
+  {
+    "revision": "e415578fd5a006fbe0acc232daa7dcde",
+    "url": "/livour-web/static/media/tripadvisor-white.e415578f.svg"
+  },
+  {
+    "revision": "4efd9a10cce45c09df0a548006e86d9b",
+    "url": "/livour-web/static/media/youtube-white.4efd9a10.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/livour-web/precache-manifest.673832107d37477de225e3086f1a6370.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/livour-web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
